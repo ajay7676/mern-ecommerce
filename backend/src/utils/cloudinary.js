f2bc77b57c2fae0afc11b6b423aeae635de40cfa
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2026-06-08 09:48:42 +0530

    Created utils folder for upload image on cloudinary & genrate slug for product

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,1 +1,29 @@
-import { v2 as cloudinary } from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary';
+
+ cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadImageToCloudinary = async(filePath, folder = "mern-ecommerce/products") => {
+      
+        const result = await cloudinary.uploader.upload(filePath, {
+        folder,
+        resource_type: "image",
+       })
+
+       return  {
+        url: result.secure_url,
+        public_id: result.public_id,
+       }
+}
+
+const deleteImageFromCloudinary = async (public_id) => {
+      if(!public_id) return null;
+       const result = await cloudinary.uploader.destroy(publicId);
+      return result;
+}
+
+export { cloudinary, uploadImageToCloudinary, deleteImageFromCloudinary };
+
