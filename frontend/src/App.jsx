e295bf705a1c1b780b34686c483baef00a87366e
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2026-06-25 17:02:22 +0530

    I have created Navbar & footer folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,10 +22,10 @@ console.log({
 
   return (
     <>
-    <button
+    {/* <button
       className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
       Button
-    </button>
+    </button> */}
     </>
   )
 }
