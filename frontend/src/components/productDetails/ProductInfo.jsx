f28181b782e0158d014942ba58323f881b3df05f
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2026-06-27 14:10:44 +0530

    Implement ProductActions components in product detail page

--- frontend/src/components/productDetails/ProductInfo.jsx
+++ frontend/src/components/productDetails/ProductInfo.jsx
@@ -4,23 +4,22 @@ import ColorSelector from "./ColorSelector";
 import SizeSelector from "./SizeSelector";
 import QuantitySelector from "./QuantitySelector";
 import ProductActions from "./ProductActions";
-import {calculateDiscount} from '../../utils/calculateDiscount'
+import { calculateDiscount } from "../../utils/calculateDiscount";
+import { useState } from "react";
 
-
-const ProductInfo = ({product}) => {
-    const finalPrice = product?.discountPrice || product?.price;
-    const discountPrice  = calculateDiscount(product.price, product.discountPrice);
+const ProductInfo = ({ product }) => {
+  const [quantity, setQuantity] = useState(1);
+  const finalPrice = product?.discountPrice || product?.price;
+  const discountPrice = calculateDiscount(product.price, product.discountPrice);
   return (
     <div className="flex flex-col">
-      <p className="font-bold text-slate-700">
-         {product?.brand || "Brand"}
-      </p>
+      <p className="font-bold text-slate-700">{product?.brand || "Brand"}</p>
       <h1 className="text-2xl font-semibold text-slate-700 mt-1">
         {product?.name}
       </h1>
-       <div className="flex items-center gap-3 mt-4 text-sm text-slate-600">
+      <div className="flex items-center gap-3 mt-4 text-sm text-slate-600">
         <span className="flex items-center gap-1 font-semibold text-slate-700">
-         {product?.ratings || 0} <FaStar className="text-orange-400" />
+          {product?.ratings || 0} <FaStar className="text-orange-400" />
         </span>
         <span>|</span>
         <span>1.1K Ratings</span>
@@ -32,16 +31,12 @@ const ProductInfo = ({product}) => {
         <p className="text-sm text-slate-500 mt-1">
           MRP <span className="line-through">₹{product?.price}</span>{" "}
           <span className="text-red-500 font-bold ml-2">
-            {
-              discountPrice > 0
-              ? `${discountPrice}% OFF`
-              : "" 
-            }
-            </span>
+            {discountPrice > 0 ? `${discountPrice}% OFF` : ""}
+          </span>
         </p>
         <p className="text-xs text-slate-500 mt-1">Inclusive of all taxes</p>
       </div>
-       {product?.description && (
+      {product?.description && (
         <p className="text-sm text-slate-600 leading-6 mt-5">
           {product.description}
         </p>
@@ -55,10 +50,14 @@ const ProductInfo = ({product}) => {
       <OfferBox />
       <ColorSelector />
       <SizeSelector />
-      <QuantitySelector stock={product?.stock} />
-      <ProductActions product={product} />
+      <QuantitySelector
+        quantity={quantity}
+        setQuantity={setQuantity}
+        stock={product?.stock}
+      />
+      <ProductActions quantity={quantity} product={product} />
     </div>
-  )
-}
+  );
+};
 
-export default ProductInfo
+export default ProductInfo;
--- frontend/src/components/productDetails/QuantitySelector.jsx
+++ frontend/src/components/productDetails/QuantitySelector.jsx
@@ -1,15 +1,56 @@
-const QuantitySelector = ({stock}) => {
+import { FiMinus, FiPlus } from "react-icons/fi";
+
+const QuantitySelector = ({ quantity, setQuantity, stock = 0 }) => {
+  const isOutOfStock = stock <= 0;
+  const isMinusDisabled = quantity <= 1;
+  const isPlusDisabled = quantity >= stock || isOutOfStock;
+  
+  const decreaseQuantity  = () => {
+    if(quantity > 1){
+       setQuantity(quantity - 1)
+    }
+
+  }
+
+  const increaseQuantity  = () => {
+    if( quantity < stock){
+       setQuantity(quantity + 1)
+    }
+
+  }
   return (
     <div className="mt-6 flex items-center gap-4">
       <p className="text-sm font-bold text-slate-700">Quantity:</p>
 
       <div className="flex items-center border border-slate-300  rounded-lg overflow-hidden">
-        <button className="w-9 h-9 bg-slate-100 hover:bg-slate-200 transition-all duration-300 ease-in-out cursor-pointer">−</button>
-        <span className="w-10 text-center text-sm font-semibold">1</span>
-        <button className="w-9 h-9 bg-slate-100 hover:bg-slate-200 transition-all duration-300 ease-in-out cursor-pointer">+</button>
+        <button className="w-9 h-9 bg-slate-100 hover:bg-slate-200 flex justify-center items-center
+         transition-all text-center duration-300 ease-in-out cursor-pointer disabled:cursor-not-allowed disabled:opacity-40"
+         onClick={decreaseQuantity}
+         disabled={isMinusDisabled}
+         >
+          <FiMinus />
+        </button>
+        <span className="w-10 text-center text-sm font-semibold">{quantity}</span>
+        <button className="w-9 h-9 bg-slate-100 hover:bg-slate-200 flex justify-center items-center
+         transition-all duration-300 ease-in-out cursor-pointer disabled:cursor-not-allowed disabled:opacity-40"
+          onClick={increaseQuantity}
+          disabled={isPlusDisabled}
+         >
+          <FiPlus />
+        </button>
       </div>
 
-      <p className="text-sm text-red-500 font-semibold">Only {stock} left in stock!</p>
+       <p
+        className={`text-sm font-semibold  ${
+          isOutOfStock ? "text-red-500" : "text-green-600"
+        }`}
+      >
+        {isOutOfStock
+          ? "Out of stock"
+          : stock <= 5
+          ? `Only ${stock} left in stock!`
+          : "In stock"}
+      </p>
     </div>
   );
 };
